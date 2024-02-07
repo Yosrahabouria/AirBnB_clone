8d509e72f0bb13124397eb98a59ef4cb4d557f49
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-02-07 15:48:37 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,5 @@
 from models.engine.file_storage import FileStorage
 
 """instance of fileStorage with Storage"""
-Storage = FileStorage()
-Storage.reload()
+storage = FileStorage()
+storage.reload()
