d7db67c5848e41236d472ae09f4a4ce8045c6389
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-10 11:48:44 -0800

    Airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -57,6 +57,6 @@ class BaseModel():
                 setattr(self, i, j)
             else:
                 self.id = str(uuid.uuid4())
-                self.creted_at = datetime.now()
+                self.created_at = datetime.now()
                 self.updated_at = datetime.now()
                 models.storage.new(self)
