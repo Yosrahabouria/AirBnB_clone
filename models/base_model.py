d36bee8b3928f6489ceb55ed252abd55cb6e56ec
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-02-10 20:40:25 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,20 +8,19 @@ from datetime import datetime
 import models
 
 
-class BaseModel():
-    """Parent class tu use in other classes
+class BaseModel:
+    """Parent class to use in other classes
     Attributes:
-          created_at: input attribute
-          updated_at: input attribute
-          __str__: input attribute
-          save: input attribute
-          to_dict: input attribute
+        created_at: input attribute
+        updated_at: input attribute
+        __str__: input attribute
+        save: input attribute
+        to_dict: input attribute
     """
 
     def __str__(self):
         """method that prints"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                       self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -36,27 +35,29 @@ class BaseModel():
         returns a dictionary containing all keys/values of __dict__
         """
         ins_dict = dict(self.__dict__)
-        ins_dict['created_at'] = self.__dict__['created_at'].isoformat()
-        ins_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        ins_dict['created_at'] = self.created_at.isoformat()
+        ins_dict['updated_at'] = self.updated_at.isoformat()
         ins_dict['__class__'] = self.__class__.__name__
-        return (ins_dict)
+        return ins_dict
 
     def __init__(self, *args, **kwargs):
         """
-        inistialization
+        initialization
         args:
             *args: input value
-            ***kwargs: input value
+            **kwargs: input value
         """
-        if kwargs:
-            kwargs['created_at'] = datetime.strptime(kwargs['crated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
 
-        for i, j in kwargs.items():
-            if i != '__class__':
-                setattr(self, i, j)
-            else:
-                self.id = str(uuid.uuid4())
-                self.creted_at = datetime.now()
-                self.updated_at = datetime.now()
-                models.storage.new(self)
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
