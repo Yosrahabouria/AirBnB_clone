440bc1bc5836b36a64ae2e5b5cf0babff75667e3
Yosrahabouria <yosrahabouria@gmail.com>
2024-02-10 10:17:29 -0800

    Airbnb clone

---
+++ pycodestyle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""passes the pycodestyle checks"""
+
+def Calcul_sum(a, b):
+    """Calculation of the sum"""
+
+    return a + b
+
+def main():
+    """The main function"""
+
+    a = float(input("Insert your first number: ")
+    b = float(input("Insert your second number: ")
+
+    S = def Calcul_sum(a, b)
+    print(f"the sum of {a} and {b} is {s}")
+
+if __name__ == "__main__":
+main()
